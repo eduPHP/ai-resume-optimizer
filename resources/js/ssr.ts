9422a824a11489062d8fe3e5f682849a8101bd3d
Edu <edu@rdo.blog.br>
2025-06-21 12:17:45 -0300

    Fix some ts errors

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -2,7 +2,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
 import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, h } from 'vue';
+import { createSSRApp, DefineComponent, h } from 'vue';
 import { route as ziggyRoute } from 'ziggy-js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -12,24 +12,28 @@ createServer((page) =>
         page,
         render: renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob('./pages/**/*.vue')),
+        resolve: (name) => resolvePageComponent(`./pages/${name}.vue`, import.meta.glob<DefineComponent>('./pages/**/*.vue')),
         setup({ App, props, plugin }) {
             const app = createSSRApp({ render: () => h(App, props) });
 
             // Configure Ziggy for SSR...
             const ziggyConfig = {
-                ...page.props.ziggy,
-                location: new URL(page.props.ziggy.location),
+                ...(page.props.ziggy || {}),
+                // @ts-expect-error location might not be defined
+                location: new URL(page.props.ziggy?.location),
             };
 
-            // Create route function...
+            // Create a route function...
+            // @ts-expect-error mismatch on return type
             const route = (name: string, params?: any, absolute?: boolean) => ziggyRoute(name, params, absolute, ziggyConfig);
 
-            // Make route function available globally...
+            // Make the route function available globally...
+            // @ts-expect-error type mismatch
             app.config.globalProperties.route = route;
 
-            // Make route function available globally for SSR...
+            // Make the route function available globally for SSR...
             if (typeof window === 'undefined') {
+                // @ts-expect-error global is undefined on frontend
                 global.route = route;
             }
 
--- resources/js/types/ziggy.d.ts
+++ resources/js/types/ziggy.d.ts
@@ -1,6 +1,7 @@
 import { RouteParams, Router } from 'ziggy-js';
 
 declare global {
+    const route: (name: string, params?: any, absolute?: boolean) => string;
     function route(): Router;
     function route(name: string, params?: RouteParams<typeof name> | undefined, absolute?: boolean): string;
 }
