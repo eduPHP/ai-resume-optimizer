36492702b0fa5da25b479a0e7cf7e5541f69aad3
edu <edu@rdo.blog.br>
2025-06-26 04:09:26 -0300

    update UX

--- resources/js/components/DeleteOptimization.vue
+++ resources/js/components/DeleteOptimization.vue
@@ -1,13 +1,14 @@
 <script lang="ts" setup>
-
 import { Trash } from 'lucide-vue-next';
 import {
-    Dialog, DialogClose,
+    Dialog,
+    DialogClose,
     DialogContent,
     DialogDescription,
     DialogFooter,
     DialogHeader,
-    DialogTitle, DialogTrigger
+    DialogTitle,
+    DialogTrigger,
 } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { OptimizationType, useOptimizationWizardStore } from '@/stores/OptimizationWizardStore';
@@ -15,37 +16,34 @@ import { router } from '@inertiajs/vue3';
 import { deleteOptimization } from '@/lib/axios';
 import { useToastsStore } from '@/stores/ToastsStore';
 
-const state = useOptimizationWizardStore()
+const state = useOptimizationWizardStore();
 const toast = useToastsStore();
 
 defineProps<{
     size?: 'lg' | 'default' | 'sm' | 'icon';
 }>();
 
-
 const handleDeleteOptimization = () => {
-    deleteOptimization()
-    state.setOptimization({} as OptimizationType)
-    toast.success('Removed!', 'The optimization was successfully removed.')
+    deleteOptimization();
+    state.setOptimization({} as OptimizationType);
+    toast.success('Removed!', 'The optimization was successfully removed.');
 
-
-    router.visit('/dashboard', { method: 'get', preserveState: true })
-}
+    router.visit('/dashboard', { method: 'get', preserveState: true });
+};
 </script>
 
 <template>
     <Dialog>
         <DialogTrigger as-child>
-            <Button :disabled="state.loading" variant="destructive" type="button" :size="size ?? 'lg'">
-                <Trash /> Remove
+            <Button :disabled="state.loading" variant="destructive" type="button" :size="size ?? 'default'">
+                <Trash />
+                Remove{{ state.form.status === 'draft' ? ' Draft' : '' }}
             </Button>
         </DialogTrigger>
         <DialogContent>
             <DialogHeader class="space-y-3">
                 <DialogTitle>Are you sure you want to delete this optimization?</DialogTitle>
-                <DialogDescription>
-                    Once it is deleted, all of its resources and data will also be permanently deleted.
-                </DialogDescription>
+                <DialogDescription> Once it is deleted, all of its resources and data will also be permanently deleted. </DialogDescription>
             </DialogHeader>
             <DialogFooter class="gap-2">
                 <DialogClose as-child>
--- resources/js/components/ui/steps/Buttons.vue
+++ resources/js/components/ui/steps/Buttons.vue
@@ -3,6 +3,7 @@ import { useOptimizationWizardStore } from '@/stores/OptimizationWizardStore';
 import { Button } from '@/components/ui/button';
 import DeleteOptimization from '@/components/DeleteOptimization.vue';
 import { cancelOptimizationEdit } from '@/lib/axios';
+import { X } from 'lucide-vue-next';
 
 defineProps<{
     action: () => void
@@ -16,18 +17,18 @@ const cancelEdit = () => {
 
 </script>
 <template>
-    <div class="mt-2 xl:mt-0 flex xl:justify-end xl:items-center gap-2 xl:gap-4">
-        <div class="flex-1" v-if="state.step > 0 || state.form.status === 'editing'">
+    <div class="mt-2 xl:mt-0 flex flex-col-reverse xl:flex-row xl:justify-end xl:items-center gap-2 xl:gap-4">
+        <div class="flex-1 flex gap-4 justify-start" v-if="state.step > 0 || state.form.status === 'editing'">
             <DeleteOptimization />
+
+            <Button v-if="state.form.response.compatibility_score"
+                    :disabled="state.loading"
+                    type="button"
+                    class="flex-1 xl:flex-none"
+                    variant="outline"
+                    @click.prevent="cancelEdit"
+            ><X /> Cancel Edit</Button>
         </div>
-        <Button v-if="state.form.status === 'editing'"
-                :disabled="state.loading"
-                type="button"
-                size="lg"
-                class="flex-1 xl:flex-none"
-                variant="outline"
-                @click.prevent="cancelEdit"
-        >Cancel</Button>
 
         <Button type="button"
                 size="lg"
