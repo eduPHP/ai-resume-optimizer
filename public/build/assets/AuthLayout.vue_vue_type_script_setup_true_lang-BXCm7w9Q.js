d1acdb0e3cdb202033195ecc9a0f8833b6b054fd
Eduardo Dalla Vecchia <edu@rdo.blog.br>
2025-07-01 23:12:10 -0300

    Rename Resume wizard to OptimizationWIzard (#17)
    * rename ResumeWizard to OptimizationWizard in Optimization.vue
    
    * chore: update Vite build (auto-commit from pre-push)

--- public/build/assets/AuthLayout.vue_vue_type_script_setup_true_lang-BXCm7w9Q.js
+++ public/build/assets/AuthLayout.vue_vue_type_script_setup_true_lang-BXCm7w9Q.js
@@ -1,4 +1,4 @@
-import{c as d}from"./Button.vue_vue_type_script_setup_true_lang-BaQx1LLH.js";import{_ as p}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-CiRaR3vS.js";import{d as a,c as _,o as c,b as t,z as i,a as o,w as r,t as s,u as f,P as m,j as u}from"./app-DuBOxP87.js";/**
+import{c as d}from"./Button.vue_vue_type_script_setup_true_lang-DwEawujN.js";import{_ as p}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-KaWTFmvv.js";import{d as a,c as _,o as c,b as t,z as i,a as o,w as r,t as s,u as f,P as m,j as u}from"./app-BQ5P6Bbp.js";/**
  * @license lucide-vue-next v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
