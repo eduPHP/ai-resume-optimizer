2a643e51a1421baa8f3019595b356ef799bb5d2a
Edu <edu@rdo.blog.br>
2025-07-15 15:19:47 -0300

    add Footer component to layouts

---
+++ resources/js/components/Footer/Footer.vue
@@ -0,0 +1,8 @@
+<template>
+    <footer class="pt-10 pb-2 text-center text-sm text-gray-400">
+        Made with ❤️ by {{' '}}
+        <a href="https://rdo.blog.br" target="_blank" class="text-blue-200 underline">
+            edu@rdo
+        </a>
+    </footer>
+</template>;
--- resources/js/layouts/app/AppSidebarLayout.vue
+++ resources/js/layouts/app/AppSidebarLayout.vue
@@ -4,6 +4,7 @@ import AppShell from '@/components/AppShell.vue';
 import AppSidebar from '@/components/AppSidebar.vue';
 import AppSidebarHeader from '@/components/AppSidebarHeader.vue';
 import type { BreadcrumbItemType } from '@/types';
+import Footer from '@/components/Footer';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -20,6 +21,7 @@ withDefaults(defineProps<Props>(), {
         <AppContent variant="sidebar">
             <AppSidebarHeader :breadcrumbs="breadcrumbs" />
             <slot />
+            <Footer />
         </AppContent>
     </AppShell>
 </template>
