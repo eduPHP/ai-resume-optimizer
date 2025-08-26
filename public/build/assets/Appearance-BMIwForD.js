559cf89b2e9bffa96b2efa650256298dc4ac0a93
eduPHP <12203605+eduPHP@users.noreply.github.com>
2025-08-26 08:01:18 +0000

    Build: Automated build update after PR merge [skip ci]

--- public/build/assets/Appearance-BMIwForD.js
+++ public/build/assets/Appearance-BMIwForD.js
@@ -1,4 +1,4 @@
-import{d as u,A as y,c as i,o as t,F as g,i as x,n as m,b as n,j as h,l as b,t as f,u as r,w as d,a as e,m as v}from"./app-BpvkVQzf.js";import{c}from"./createLucideIcon-CHwD2fBo.js";import{_ as M,a as w}from"./Layout.vue_vue_type_script_setup_true_lang-uLYDoLJe.js";import{_ as A}from"./AppLayout.vue_vue_type_script_setup_true_lang-Dm9cBZm5.js";import"./Label.vue_vue_type_script_setup_true_lang-CVKxDWQA.js";import"./check-CcMNCJf6.js";import"./AppLogoIcon.vue_vue_type_script_setup_true_lang-BrOYjUA-.js";/**
+import{d as u,A as y,c as i,o as t,F as g,i as x,n as m,b as n,j as h,l as b,t as f,u as r,w as d,a as e,m as v}from"./app-CKxH55pF.js";import{c}from"./createLucideIcon-Cv2livCt.js";import{_ as M,a as w}from"./Layout.vue_vue_type_script_setup_true_lang-BlGpGA7G.js";import{_ as A}from"./AppLayout.vue_vue_type_script_setup_true_lang-CL_5eSZB.js";import"./Label.vue_vue_type_script_setup_true_lang-pnCHqD5a.js";import"./check-Z3FlHBhX.js";import"./AppLogoIcon.vue_vue_type_script_setup_true_lang--6TDztCK.js";/**
  * @license lucide-vue-next v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
