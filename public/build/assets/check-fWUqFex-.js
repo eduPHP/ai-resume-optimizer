c8cf2f69fc45230820dcef1563f74b36c8f2616c
eduPHP <12203605+eduPHP@users.noreply.github.com>
2025-08-29 12:58:17 +0000

    Build: Automated build update after PR merge [skip ci]

--- public/build/assets/check-fWUqFex-.js
+++ public/build/assets/check-fWUqFex-.js
@@ -1,4 +1,4 @@
-import{c}from"./createLucideIcon-C0ZZHnvV.js";/**
+import{c}from"./loader-circle-CDdhhIU0.js";/**
  * @license lucide-vue-next v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
