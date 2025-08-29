85dbe20fb74a082584a89cec829e1719ecf622ac
eduPHP <12203605+eduPHP@users.noreply.github.com>
2025-08-29 00:57:20 +0000

    Build: Automated build update after PR merge [skip ci]

--- public/build/assets/AuthLayout.vue_vue_type_script_setup_true_lang-D4vMM3EX.js
+++ public/build/assets/AuthLayout.vue_vue_type_script_setup_true_lang-D4vMM3EX.js
@@ -1,4 +1,4 @@
-import{c as d}from"./createLucideIcon-zQmsuDI4.js";import{_ as p}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-Co9nAlBL.js";import{d as a,c as _,o as c,b as t,z as i,a as o,w as r,t as s,u as f,P as m,j as u}from"./app-DjfxXyiZ.js";/**
+import{c as d}from"./createLucideIcon-CZIA5eF0.js";import{_ as p}from"./AppLogoIcon.vue_vue_type_script_setup_true_lang-CTYgNxfs.js";import{d as a,c as _,o as c,b as t,z as i,a as o,w as r,t as s,u as f,P as m,j as u}from"./app-9o6MmVwQ.js";/**
  * @license lucide-vue-next v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
