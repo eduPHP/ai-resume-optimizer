a1674291213949528eb891a0694fab01fd647ab7
Edu <edu@rdo.blog.br>
2025-09-07 01:46:24 -0300

    Add `role` column to users table with migration, update first user as admin, and introduce `isAdmin` method in User model

---
+++ database/migrations/2025_09_07_040331_alter_users_add_role.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('user')->after('remember_token');
+        });
+        // update first user if it exists
+        DB::table('users')->where('id', 1)->update(['role' => 'admin']);
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+        });
+    }
+};
