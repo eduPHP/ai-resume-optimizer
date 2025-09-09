2b703190a848bb29a271e138256cae57e8b4f09e
Edu <edu@rdo.blog.br>
2025-09-09 09:47:12 -0300

    fix next step on role info

--- resources/js/components/ui/steps/RoleInformation.vue
+++ resources/js/components/ui/steps/RoleInformation.vue
@@ -28,6 +28,7 @@ const submit = async () => {
             state.setOptimization(response.data.optimization as OptimizationType)
             nav.addItem(response.data.optimization.role_company, response.data.optimization.id as string, true)
             router.visit(route('optimizations.show', response.data.optimization.id), { preserveState: true })
+            state.nextStep()
         }
     })
 }
