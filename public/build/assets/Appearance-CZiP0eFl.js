b25a46b60255c07cafadd3da0050bc8883fd206b
eduPHP <12203605+eduPHP@users.noreply.github.com>
2025-09-11 09:31:02 +0000

    Build: Automated build update after PR merge [skip ci]

--- public/build/assets/Appearance-CZiP0eFl.js
+++ public/build/assets/Appearance-CZiP0eFl.js
@@ -1,4 +1,4 @@
-import{d as u,A as y,c as i,o as t,F as g,i as x,n as m,b as n,j as h,l as b,t as f,u as r,w as d,a as e,m as v}from"./app-zFuM9G68.js";import{c}from"./loader-circle-DVv48EtV.js";import{_ as M,a as w}from"./Layout.vue_vue_type_script_setup_true_lang-B1RRFQQt.js";import{_ as A}from"./AppLayout.vue_vue_type_script_setup_true_lang-BsXywWgD.js";import"./Label.vue_vue_type_script_setup_true_lang-BxFbuEby.js";import"./check-Cg1Cwl_M.js";import"./AppLogoIcon.vue_vue_type_script_setup_true_lang-D7jIEsX3.js";/**
+import{d as u,A as y,c as i,o as t,F as g,i as x,n as m,b as n,j as h,l as b,t as f,u as r,w as d,a as e,m as v}from"./app-BM7hDGx7.js";import{c}from"./loader-circle-ss2pAM87.js";import{_ as M,a as w}from"./Layout.vue_vue_type_script_setup_true_lang-CNQJmmFI.js";import{_ as A}from"./AppLayout.vue_vue_type_script_setup_true_lang-BUyBQbU4.js";import"./Label.vue_vue_type_script_setup_true_lang-Dr3OJ5Mu.js";import"./check-NSVmD8N5.js";import"./AppLogoIcon.vue_vue_type_script_setup_true_lang-CJ3UejzI.js";/**
  * @license lucide-vue-next v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- public/build/assets/check-NSVmD8N5.js
+++ public/build/assets/check-NSVmD8N5.js
@@ -1,4 +1,4 @@
-import{c}from"./loader-circle-DVv48EtV.js";/**
+import{c}from"./loader-circle-ss2pAM87.js";/**
  * @license lucide-vue-next v0.468.0 - ISC
  *
  * This source code is licensed under the ISC license.
